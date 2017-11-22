eb2b0a7db73773fb77a3cc63d8ee9275bbd42dc5
ClemenceL <clemence.lievre@gmail.com>
2017-11-22 17:14:03 +0100

    Add a player class

---
+++ TPM2_WoZ5/src/tpm2_woz5/Player.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpm2_woz5;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author clemence
+ */
+public class Player {
+
+    private String pseudoPlayer;
+    private ArrayList<Weapon> weaponPlayer;
+    private int strengthPlayer;
+
+    public Player(String pseudo, ArrayList<Weapon> arme, int force) {
+        this.pseudoPlayer = pseudo;
+        this.weaponPlayer = arme;
+        this.strengthPlayer = force;
+    }
+
+    public String getPseudo() {
+        return pseudoPlayer;
+    }
+
+    public ArrayList<Weapon> getArme() {
+        return weaponPlayer;  
+    }
+
+    public int getForce() {
+        return strengthPlayer;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudoPlayer = pseudo;
+    }
+
+    public void setArme(ArrayList<Weapon> arme) {
+        this.weaponPlayer = arme;
+    }
+
+    public void setForce(int force) {
+        this.strengthPlayer = force;
+    }
+
+}
